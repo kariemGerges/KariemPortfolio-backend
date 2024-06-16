f5afad5b2d9b2f9dcca9c38985de8ea246d56207
kgerges <kgerges@iu.edu>
2024-06-16 11:45:54 -0400

    fixing port issues

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(cors());
 app.use('/api/v1', apiRouter);
 app.use('/api/v1', apiRouterV2);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
